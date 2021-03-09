cc5cda37b28ab6b91f6084dc1ccbf1e5a7b8226c
Luke <zenglingkun168@gmail.com>
2021-03-09 14:48:29 -0800

    add application scan

--- src/main/java/com/montetech/seckill/SeckillApplication.java
+++ src/main/java/com/montetech/seckill/SeckillApplication.java
@@ -1,9 +1,13 @@
 package com.montetech.seckill;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@MapperScan("com.montetech.seckill.db.mappers")
+@ComponentScan(basePackages = {"com.montetech"})
 public class SeckillApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/montetech/seckill/db/mappers/SeckillCommodityMapper.java
@@ -0,0 +1,17 @@
+package com.montetech.seckill.db.mappers;
+
+import com.montetech.seckill.db.po.SeckillCommodity;
+
+public interface SeckillCommodityMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(SeckillCommodity record);
+
+    int insertSelective(SeckillCommodity record);
+
+    SeckillCommodity selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(SeckillCommodity record);
+
+    int updateByPrimaryKey(SeckillCommodity record);
+}
